11fa1688aa6b5b628057740af166fdc525aa7d28
Stephen Gerard <orawiz@dodo.com.au>
2014-05-22 11:12:50 +1000

    Assignment 2
    Programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are designed to remember or cache a 
+## previous matrix inverse result.
+## An inverse matrix calculation can be expensive to prepare.
+## When a previous calculation is available in cache this is provided
+## instead of calculating again.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function manages the cached matrix inverse result.
+## This function provides a way to set and get the inverse result provided by cacheSolve.
+## This function references a parent environment to locate the inverse result.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function( invert) inv <<- invert
+        getinv <- function() inv
+        list( set = set, get = get,
+              setinv = setinv,
+              getinv = getinv)         
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function calculates a square matrix inverse using the solve function
+## When available provides the previous result saved to cache 
+## Delegates to makeCacheMatrix to maintain the cache
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+        
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data)
+        x$setinv(inv)
+        inv
 }
